d02f98fb967047abbfce63acc17614dfdffaa96a
VICTOR DANIEL JESUS DE SOUSA <victor.sousa007@cs.udf.edu.br>
2026-09-15 10:48:56 -0300

    feat : adicionar cabeçalho

---
+++ calculadora.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int main(){
+    int opcao;
+    float num1 , num2, resultado;
+ 
+    printf("========================");
+    printf("    Calculadora\n"       );
+    printf("========================");
+
+    printf("Escolha a opereçao\n");
+    printf("1- Soma\n");
+    printf("2- Subtraçao\n");
+    printf("3- multiplicaçao\n");
+
+    printf("\nDigie sua escolha");
+    scanf("%d" &opcao);
+
+    printf("\n Escolha o primeiro numero");
+    scanf("%f" &num1);
+
+    printf("\n Escolha o segundo numero");
+    scanf("%f" &num2);
+
+
+   
+}
