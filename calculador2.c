a9b03fb2aa36f752d66cbfc6a8cdb64af30b8eea
Arthur <arthurbezerra868@gmail.com>
2026-09-15 14:08:56 -0300

    Adiciona multiplicação

--- calculador2.c
+++ calculador2.c
@@ -3,5 +3,9 @@
         resultado = num1 + num2;
         printf("\nResultado e  %.2f\n" &resultado);
         break;
-
+           
+        case 3:
+        resultado = num1 * num2;
+        printf("\nResultado da multiplicacao: %.2f\n", resultado);
+        break;
     }
