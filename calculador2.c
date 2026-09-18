7604d0718ecb7845dde67b5c9f4db5478a47542c
Victor Daniel <vitinmuitobom@gmail.com>
2026-09-18 09:37:22 -0300

    Update calculador2.c

--- calculador2.c
+++ calculador2.c
@@ -1,9 +1,4 @@
-    switch (opcao){
-        case 1:
-        resultado = num1 + num2;
-        printf("\nResultado e  %.2f\n" &resultado);
-        break;
-           
+
         case 3:
         resultado = num1 * num2;
         printf("\nResultado da multiplicacao: %.2f\n", resultado);
